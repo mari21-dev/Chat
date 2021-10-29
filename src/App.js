0fffa5c8101e1abc11fcce79a389db799b1a7497
Marina Dubinina <dubinina.m.21@gmail.com>
2021-10-29 18:38:53 +0300

    Add useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Message from "./components/Message";
+import Messages from "./components/Messages";
 
 const message = 'Hello world!';
 
 function App() {
-  return (
-   <Message message={message}/>
-  );
+    return (
+        // <Message message={message}/>,
+            <Messages/>
+    )
+        ;
 }
 
 export default App;
